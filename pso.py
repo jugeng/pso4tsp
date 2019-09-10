c02698b98a1797ab39e4cb2319e92299b77d5cd0
Jugen Gawande Desktop <jugengawande@gmail.com>
2019-09-10 21:57:36 +0530

    Crossover funcation and Reverse Function added

--- pso.py
+++ pso.py
@@ -14,15 +14,17 @@ cities = [] # Empty array of cities
 distance_matrix = []
 gBest_fitness = 0
 gBest_route = []
-
+iteration = 0
 
 #learning rate
 c1 = 0
 c2= 0
-
+""""
 x = [5,10,12,2,14,15,3,6,8]
 y = [6,5,11,1,6,10,8,6,10,2]
-
+"""
+x = [1,2,3,1,3,4,5]
+y = [3,3,3,1,1,1,1]
 
 class Particle:
     
@@ -49,7 +51,8 @@ class Particle:
         total_distance = 0
         global gBest_fitness
 
-        for i in range(TotalCities):  total_distance = total_distance  + distance_matrix[self.route[i]][self.route[i+1]]
+        for i in range(TotalCities):  
+            total_distance = total_distance  + distance_matrix[self.route[i]][self.route[i+1]]
         
         #Using inverse because shorter the distance the better
         self.fitness = 1 / total_distance
@@ -69,32 +72,51 @@ class Particle:
             gBest_route.extend(self.route)
             print("MSG: Global best updated ->", gBest_fitness,"-- route ->",gBest_route)
         
-        def Crossover():
-            int_route = []
-            
-            k = random.randint(1,TotalCities+1)
-            #Crossover length
-            m = c1 * random.randint(1,TotalCities+1)    #For Local Best
-            n = c1 * random.randint(1,TotalCities+1)    #For Global Best
+    def Crossover(self):
+        int_route = []
+        
+        k = random.randint(1,TotalCities-1)
+        #Crossover length
+        m = c1 * random.randint(1,TotalCities-1) % TotalCities   #For Local Best
+        n = c1 * random.randint(1,TotalCities-1) % TotalCities    #For Global Best
 
-            int_route.append(0)
+        int_route.append(0)
+        #Crossover between pbest and gbest
+        for x in range(n):
 
-            for x in range(n):
-                if k + x > TotalCities:
-                    r = (k + x) % TotalCities
-                    int_route.append(gBest_route[r])
-                else:
-                    int_route.append(gBest_route[k+x]) 
+            if (k + x) >= TotalCities:
+                
+                r = (k + x) % TotalCities + 1
+                int_route.append(gBest_route[r])
+         
+            else:
+                int_route.append(gBest_route[k+x]) 
+    
+        #Cross over between pbest and x
+        for x in range(m):
+            if (k + x) >= TotalCities:
+                r = (k + x) % TotalCities +1 
+                int_route.append(self.pBest_route[r]) if self.pBest_route[r] not in int_route else int_route
+            else:
+                int_route.append(self.pBest_route[k+x]) if self.pBest_route[k+x] not in int_route else int_route 
+  
+        for x in range(len(self.route)): 
+            int_route.append(self.route[x]) if self.route[x] not in int_route else int_route
+        
+        self.route = int_route[:]
+        self.route.append(0)
 
-            for x in range(m):
-                if k + x > TotalCities:
-                    r = (k + x) % TotalCities
-                    int_route.append(self.pBest_route[r])
-                else:
-                    int_route.append(self.pBest_route[k+x]) 
-            
-            for x in range(len(route)): 
-                int_route.append(self.route[x]) if self.route[x] not in int_route else int_route
+    def Variation(self):
+        k = random.random()
+        if(k > 0.5):
+            reverseVar()
+
+
+    def reverseVar(self):
+        s = random.randint(1,TotalCities / 2) % TotalCities
+        e = random.randint(s,TotalCities) % TotalCities
+
+        self.route[s:e] = reversed(self.route[s:e]) 
 
             
 
@@ -142,22 +164,33 @@ class PSO:
         if len(self.swarm) == PopulationSize:
             print('CHECK: Intial population generated successfully')
 
-    def ShowFitness(self):
         for i in range(PopulationSize):
             self.swarm[i].CalculateFitness()
 
-        #print("MSG: Global best updated ->", gBest_fitness,"-- route ->",gBest_route)   
+
+    def runAlgo(self):
+        for j in range(iteration):
+                   
+            print("Iteration:", j+1)
+            for i in range(PopulationSize):
+                self.swarm[i].Crossover()
+                self.swarm[i].CalculateFitness()
+
+            print("RESULT: Best distance =",1 / gBest_fitness )    
+      
+       
     
 
-
+#print("MSG: Global best updated ->", gBest_fitness,"-- route ->",gBest_route)   
 
 #PopulationSize = int(input("Enter population size: "))
-PopulationSize = 5
+PopulationSize =15
 
 print('Set learning rate: {recommended-> 2}\n')
 #c1 = int(input("c1: "))
 #c2 = int(input("c2: "))
-
+#iteration = int(input("Set number of iteration: "))
+iteration = 50
 c1 = 2
 c2 = 2
 
@@ -170,4 +203,4 @@ c2 = 2
 pso = PSO()
 
 pso.GenerateSwarm()
-pso.ShowFitness()
+pso.runAlgo()
